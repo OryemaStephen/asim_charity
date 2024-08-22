21d67f4b8ba1216ff996a0a2cf2f3e8dda9dbcbe
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-08-22 19:59:59 +0300

    Setting up project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <h1>ASIM CHARITY ORGANSATION</h1>
+    </div>
+  )
+}
+
+export default App;
