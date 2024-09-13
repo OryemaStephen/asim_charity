b9f922fc6e8417f4f5a8a2192c42d777d2e2d533
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-09-13 11:01:21 +0300

    Updated more contents on the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,14 @@ import Welcome from '../components/Welcome';
 import Footer from '../components/Footer';
 import background from '../assets/6.jpg'
 import AboutImage from '../components/AboutImage';
+import HomeVolunteerForm from '../components/HomeVolunteerForm';
 
 const About = () => {
   return (
     <div className="w-full">
       <Welcome text="About the organization" image={background}  />
       <AboutImage />
+      <HomeVolunteerForm />
       <Footer />
     </div>
   );
